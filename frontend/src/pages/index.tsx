e4017de11ba035ad0a0153a0cb4e04ec24ea14e8
Ricardo Morato <rmr@cin.ufpe.br>
2023-04-29 13:18:41 -0300

    feat: add map to main page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Mapa from "@/components/Mapa";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +24,8 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
         </div>
+
+        <Mapa />
       </main>
     </>
   );

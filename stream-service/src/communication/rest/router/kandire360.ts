dab96dd055641a9b1610619af7fcdac4f76d4584
celso <jose.lima@consensotec.com.br>
2023-04-29 20:54:46 -0300

    list estados e list municipios por codUF

--- stream-service/src/communication/rest/router/kandire360.ts
+++ stream-service/src/communication/rest/router/kandire360.ts
@@ -5,7 +5,7 @@ const routerKandire360 = express.Router()
 
 routerKandire360.get('/kandire360', ListKandire360Controller)
 
-routerKandire360.get('/states', ListEstadosController)
+routerKandire360.get('/estados', ListEstadosController)
 
 routerKandire360.get('/municipiosByCodUF', listMunicipiosController)
 
